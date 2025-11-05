e66a5420384607625b42a96204be1515fff058be
ignaciobenitezv <benitezvucasignacio@gmail.com>
2025-11-05 19:10:07 -0300

    just1cfg

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    // whitelist directo
+    domains: ["cdn.sanity.io", "images.sanitycdn.com"],
+    // y por si tu build usa remotePatterns
+    remotePatterns: [
+      { protocol: "https", hostname: "cdn.sanity.io", pathname: "/images/**" },
+      { protocol: "https", hostname: "cdn.sanity.io", pathname: "/files/**" },
+      { protocol: "https", hostname: "images.sanitycdn.com", pathname: "/**" },
+    ],
+    formats: ["image/avif", "image/webp"],
+  },
   transpilePackages: [
-    'sanity',
-    'next-sanity',
-    '@sanity/ui',
-    '@sanity/vision',
-    'styled-components'
+    "sanity",
+    "next-sanity",
+    "@sanity/ui",
+    "@sanity/vision",
+    "styled-components",
   ],
   compiler: {
-    styledComponents: true
-  }
+    styledComponents: true,
+  },
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
