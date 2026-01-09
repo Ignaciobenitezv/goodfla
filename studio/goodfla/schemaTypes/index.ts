454faf5d77520cd569ffdfef35328ad1ef85e243
Ignaciobenitezv <benitezvucasignacio@gmail.com>
2026-01-09 17:11:38 -0300

    sanityFixing

--- studio/goodfla/schemaTypes/index.ts
+++ studio/goodfla/schemaTypes/index.ts
@@ -3,5 +3,7 @@ import categoria from './categoria'
 import combo from './combo'  
 import mayorista from './mayorista'
 import zapatillas2x1 from './zapatillas2x1'
+import mediaItem from './mediaitems'
+import mediaItems from './mediaitems'
 
-export const schemaTypes = [producto, categoria, combo, mayorista, zapatillas2x1]
+export const schemaTypes = [producto, categoria, combo, mayorista, zapatillas2x1, mediaItems]
